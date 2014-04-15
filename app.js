156c47915bafbe620d6b620c699b8212490a13cd
Will V <willvedd@gmail.com>
2014-04-14 20:01:30 -0400

    Updating to newest versions of files

--- app.js
+++ app.js
@@ -1,9 +1,5 @@
 //-------------------------Model and Collection-----------------------------
-<<<<<<< HEAD
-
-=======
 console.time('load');
->>>>>>> f1c95501e6c8f30c746c876d540c9fb1f5534b08
 var Station = Backbone.Model.extend({
     defaults: {
         id: null,
@@ -24,16 +20,13 @@ var Station = Backbone.Model.extend({
 var StationList = Backbone.Collection.extend({
     model: Station,
     url: 'stations.json',
-<<<<<<< HEAD
 });
 
 var stations = new StationList();
 
-stations.fetch({
-    success: function() {
-        console.log("Successful fetch");
-    }
-});
+console.time('fetch');
+stations.fetch();
+console.timeEnd('fetch');
 
 //----------------------- Station View -------------------------------
 
@@ -45,11 +38,10 @@ var StationView = Backbone.View.extend({
 
     className: "station",
 
-    initialize: function(res) {
-        console.log("Station view initialized");
+    initialize: function() {
         this.render();
     },
-    render: function(eventName) {
+    render: function() {
         this.$el.html(this.template(this.model.toJSON()));
         this.el.value = this.model.cid;//Sets value for drop down selections, allows future binding
         return this;
@@ -58,41 +50,6 @@ var StationView = Backbone.View.extend({
 });
 
 //---------------------Stations View---------------------------------
-=======
-});
-
-var stations = new StationList();
-
-stations.fetch({
-    success: function() {
-        console.log("Successful fetch");
-    }
-});
-
-//----------------------- Station View -------------------------------
-
-var StationView = Backbone.View.extend({
-
-    tagName: 'option',
-
-    template: _.template($('#stationTemplate').html()),
-
-    className: "station",
-
-    initialize: function(res) {
-        console.log("Station view initialized");
-        this.render();
-    },
-    render: function(eventName) {
-        this.$el.html(this.template(this.model.toJSON()));
-        this.el.value = this.model.cid;//Sets value for drop down selections, allows future binding
-        return this;
-    },
-
-});
-
-//---------------------Stations View---------------------------------
-
 
 var StationListView = Backbone.View.extend({
 
@@ -102,7 +59,8 @@ var StationListView = Backbone.View.extend({
         this.render();
     },
 
-    render: function(start) {
+    render: function() {
+
         this.$el.empty(); //clears existing elements
 
         if($('#myonoffswitch').is(":checked")){
@@ -112,13 +70,17 @@ var StationListView = Backbone.View.extend({
         	var line = 'bs'
         };
 
+        this.$el.append('<option disabled="disabled" selected="selected">Select station</option>');
 
         this.collection.each(function(station) { //generates models from stations collection that correlate to correct station selection
-            if ( (station.get('line') == line)) {
-                var stationView = new StationView({
+            if ( (station.get('line') == line) ) {
+            	if(station.cid != $('#start_dest').val()){
+            		var stationView = new StationView({
                     model: station
                 });
+
                 this.$el.append(stationView.el);
+            	}
             };
         }, this); //"This" loses reference inside loop, regains it with appended ",this"
 
@@ -141,33 +103,42 @@ var GlobalView = Backbone.View.extend({
 
     el: 'body',
 
-    intitialize: function() {
-        console.log("Global init"),
-        this.render();
-    },
-
-    render: function() {
-        console.log("Global render");
-    },
-
     events: {
         'click .submit': 'submit',
         'click #wk' : 'setWeek',
         'click #sat' : 'setSat',
         'click #sun' : 'setSun',
         'click #rst': 'reset',
+        'click #reverse': 'reverse',
+        'click #fav': 'favorite',
         'change input[type=checkbox]': 'lineset',
+        'change #start_dest': 'endlineset',
+        'change #end_dest': 'startlineset',
+    },
+
+    initialize: function(){
+    	start_list.render();
+    	end_list.render();
     },
 
     submit: function() {
-
+    	console.time('submit');
         var start = start_list.getVal();
         window.start_station = stations._byCid[start];
 
         var end = end_list.getVal();
         window.end_station = stations._byCid[end];
 
-        this.schedule(start_station,end_station,day());//passing selected objects to scheduling logic
+        console.time("hide");
+        if((start_station!=undefined)&&(end_station!=undefined)){//validates the presence of data, or else the drop downs will disappear
+            console.log("hiding");
+            $('.platter').hide();
+        };
+        console.timeEnd("hide");
+
+        this.schedule(start_station,end_station,day);//passing selected objects to scheduling logic
+    
+    	console.timeEnd('submit');
     },
 
 
@@ -183,218 +154,93 @@ var GlobalView = Backbone.View.extend({
         var instruct = day+"_"+dir;
 
         var start_preschedule = start.get(instruct);
-
         var end_preschedule = end.get(instruct);
 
         window.start_schedule = [];
-
         window.end_schedule = [];
 
         var j = 0;
-
+        console.time('scheduleFor');
         for(i=0; i<(start.get(instruct).length);i++){
             if(start_preschedule[i]!=null){
                 if(end_preschedule[i]!=null){
-                    start_schedule[j] = start_preschedule[i];
-                    end_schedule[j] = end_preschedule[i];
-                    j++
+                    start_schedule[j] = timeformat(start_preschedule[i]);
+                    end_schedule[j] = timeformat(end_preschedule[i]);
+                    j++;
                 };
             };
         };
+        console.timeEnd('scheduleFor');
 
         schedule.render(start,end,day);
         console.timeEnd('schedule');
     },
 
     reset: function() {
+    	console.time('reset');
         start_list.reset();
         end_list.reset();
         toolbar.reset();
         schedule.$el.empty();
+        $('.platter').show();
+        console.timeEnd('reset');
     },
 
     setWeek: function(){
-    	console.log("Set to week");
-    	this.schedule(start_station,end_station,'wk');
+        day = 'wk';
+    	this.schedule(start_station,end_station,day);
     },
 
     setSat: function(){
-    	this.schedule(start_station,end_station,'sat');
-    	console.log("Set to saturday");
+        day = 'sat';
+    	this.schedule(start_station,end_station,day);
     },
 
     setSun: function(){
-    	this.schedule(start_station,end_station,'sun');
-    	console.log("Set to sunday");
+    	console.time('setSun');
+        day = 'sun';
+    	this.schedule(start_station,end_station,day);
+        console.timeEnd('setSun');
+    },
+
+    reverse: function(){
+        console.time('reverse');
+        temp_station = start_station;
+        start_station = end_station;
+        end_station = temp_station;
+        this.schedule(start_station,end_station,day);
+        console.timeEnd('reverse');
+    },
+
+    favorite: function(){
+        console.time("favorite");
+        console.log("Favorites: "+start_station.cid+" and "+end_station.cid);
+        $('#fav').addClass("fav_active");
+        $.cookie.write('start_fav', start_station.cid, 24 * 60 * 60 *365);
+        $.cookie.write('end_fav', end_station.cid, 24 * 60 * 60 *365);
+        console.timeEnd("favorite");
     },
 
     lineset: function() {
-        start_list.reset();
-        end_list.reset()
-        schedule.$el.empty();
-    },
-
-});
-
-
-//---------------------Schedule View---------------------------------
->>>>>>> f1c95501e6c8f30c746c876d540c9fb1f5534b08
-
-
-<<<<<<< HEAD
-var StationListView = Backbone.View.extend({
-
-    tagName: 'select',
-
-    intialize: function() {
-        this.render();
-    },
-
-    render: function(start) {
-        this.$el.empty(); //clears existing elements
-
-        var line = $('input.line:checked').val(); //checks to see what line is selected
-
-        this.collection.each(function(station) { //generates models from stations collection that correlate to correct station selection
-            if ( (station.get('line') == line)) {
-                var stationView = new StationView({
-                    model: station
-                });
-                this.$el.append(stationView.el);
-            };
-        }, this); //"This" loses reference inside loop, regains it with appended ",this"
-
-        return this;
-    },
-
-    reset: function() {
-        this.render();
-        return this;
-    },
-
-    getVal: function() {
-        return ($(this.$el).val());
-    },
-});
-
-//---------------------Global View---------------------------------
-
-var GlobalView = Backbone.View.extend({
-
-    el: 'body',
-
-
-    intitialize: function() {
-        console.log("Global init"),
-        this.render();
-    },
-
-    render: function() {
-        console.log("Global render");
-    },
-
-    events: {
-        'click .submit': 'submit',
-        'click .reset': 'reset',
-        'click input[type=radio]': 'lineset',
-        'change select': 'submit',
-    },
-
-    submit: function() {
-
-        var start = start_list.getVal();
-        window.start_station = stations._byCid[start];
-
-        var end = end_list.getVal();
-        window.end_station = stations._byCid[end];
-
-        this.schedule(start_station,end_station);//passing selected objects to scheduling logic
-    },
-=======
-    el: '#scheduleTable',
-
-    template: _.template($('#scheduleTemplate').html()),
-
-    initialize: function(){
-        this.render;
-    },
-
-    render: function(start,end,day){
-        this.$el.html(this.template(start.toJSON()));
-        toolbar.render();
-    },
-});
-
-var ToolbarView = Backbone.View.extend({
-	el: '#toolbar',
-
-	template: _.template($('#toolbarTemplate').html()),
-
-	render: function(){
-		this.$el.empty();
-		this.$el.append(this.template);
-	},
-
-	reset: function(){
-		this.$el.empty();
-	}
-})
-
-//---------------------Other code---------------------------------
->>>>>>> f1c95501e6c8f30c746c876d540c9fb1f5534b08
-
-$(function() {
-
-<<<<<<< HEAD
-    schedule: function(start,end){
-
-        if(start.get('order')<end.get('order')){
-            var dir = 'sb';
-        }
-        else{
-            var dir = 'nb';
-        }
-
-        var instruct = day()+"_"+dir;
-
-        var start_preschedule = start.get(instruct);
-
-        var end_preschedule = end.get(instruct);
-
-        window.start_schedule = [];
-
-        window.end_schedule = [];
-
-        var j = 0;
-
-        for(i=0; i<(start.get(instruct).length);i++){
-            if(start_preschedule[i]!=null){
-                if(end_preschedule[i]!=null){
-                    start_schedule[j] = start_preschedule[i];
-                    end_schedule[j] = end_preschedule[i];
-                    j++
-                };
-            };
-        };
-
-        console.log(start_schedule);
-        console.log(end_schedule);
-
-        schedule.render(start,end,day());
-    },
-
-    reset: function() {
+    	console.time('lineset');
         start_list.reset();
         end_list.reset();
-        schedule.$el.empty();
+        $('.label2').toggleClass("mf");
+        $('.label1').toggleClass("bs");
+        console.timeEnd('lineset');
     },
 
-    lineset: function() {
-        start_list.reset();
-        end_list.reset()
-        schedule.$el.empty();
+    endlineset: function(){
+    	if($('#start_dest').val()==$('#end_dest').val()){
+    		end_list.reset();
+    	};
     },
 
+    startlineset: function(){
+    	if($('#end_dest').val()==$('#start_dest').val()){
+    		start_list.reset();
+    	};
+    },
 });
 
 
@@ -412,8 +258,31 @@ var ScheduleView = Backbone.View.extend({
 
     render: function(start,end,day){
         this.$el.html(this.template(start.toJSON()));
+        $('.table-wrap').height($(window).height()-150);
+        toolbar.render();
     },
 });
+
+var ToolbarView = Backbone.View.extend({
+	el: '#toolbar',
+
+	template: _.template($('#toolbarTemplate').html()),
+
+	render: function(){
+		console.time('toolbar render');
+		this.$el.empty();
+		this.$el.append(this.template);
+        console.log("toolbar rendering");
+        console.log($("."+day));
+        $("#"+day).addClass("active");
+
+		console.timeEnd('toolbar render');
+	},
+
+	reset: function(){
+		this.$el.empty();
+	}
+})
 
 //---------------------Other code---------------------------------
 
@@ -432,9 +301,45 @@ $(function() {
         }
     };
 
+    window.day = day();
+
+    time = function(){
+    	var now = new Date();
+    	var hours = now.getHours();
+    	if (hours == 0){
+    		hours = 24;
+    	};
+    	var minutes = now.getMinutes(); 
+   		return hours+"."+minutes;//setting time 
+    }
+
+    timeformat = function(time) { //formats time from a double into hh:mm for rendering purposes only
+	    if ((time >= 12) && (time < 24)) {
+	        var meridian = "PM";
+	        if (time > 12.59) {
+	            time = ((Math.round((time-12.00) * 100)) / 100) //Formatting time from 24hr to 12hr style
+	        };
+	    } else {
+	        var meridian = "AM"
+	    };
+	    time = time.toString().split("."); //Converting time variable to a string and splitting to get minutes and hours
+	    if (time[1] === undefined) { //If minutes are undefined,
+	        time[1] = "00"; //Converts undefined minutes to hh:00;
+	    };
+	    if (time[0] == 24) { //If the hour is 24, set it to 12
+	        time[0] = 12
+	    };
+	    if (time[1].length == 1) {
+	        time[1] = time[1] * 10; //Adds trailing zero to hh:m0 numbers that otherwise display as hh:m;
+	    };
+
+	    return (time[0] + ":" + time[1] + " " + meridian); //returning a string of the time
+	};
+
     start_list = new StationListView({
         collection: stations,
         el: '#start_dest',
+
     });
 
     end_list = new StationListView({
@@ -442,42 +347,22 @@ $(function() {
         el: '#end_dest'
     });
 
+	console.time('make global view');
     global = new GlobalView();
+    console.timeEnd('make global view');
 
+    console.time('make schedule view');
     schedule = new ScheduleView();
-=======
-    day = function(){//returns "wk","sat","sun" depending on day of week
-        var day = Date().split(" "); //Gets raw day of the week
-        if (day[0] == "Sat") {
-            //return "sat";
-            return "sat";
-        } else if (day[0] == "Sun") {
-            //return "sun";
-            return "sun";
-        } else {
-            return "wk";
-        }
-    };
+    console.timeEnd('make schedule view');
 
-    start_list = new StationListView({
-        collection: stations,
-        el: '#start_dest',
-    });
-
-    end_list = new StationListView({
-        collection: stations,
-        el: '#end_dest'
-    });
-
-	console.time('make view');
-    global = new GlobalView();
-    console.timeEnd('make view');
-
-    schedule = new ScheduleView();
+    console.time('make toolbar view');
     toolbar = new ToolbarView();
+    console.timeEnd('make toolbar view');
 
     console.timeEnd('load');
->>>>>>> f1c95501e6c8f30c746c876d540c9fb1f5534b08
+
+    console.log("Start saved: "+$.cookie.read('start_fav'));
+    console.log("End saved: "+$.cookie.read('end_fav'));
 });
 
 
